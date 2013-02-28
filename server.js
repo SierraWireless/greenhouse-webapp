ea175aa3526be5345b536eb0b0d9d1bd4ca051b6
Simon Bernard <sbernard@sierrawireless.com>
2013-02-28 18:38:36 +0100

    Support host name header.

--- server.js
+++ server.js
@@ -18,16 +18,22 @@ console.log(welcome.green.bold);
 //Create a node-static server instance to serve the './app' folder
 var file = new(static.Server)('./app');
 
+// Create proxy instance
+var proxy = new httpProxy.HttpProxy({
+    target : {
+      port : 80, 
+      host : 'na.m2mop.net'},
+    changeOrigin: true, // changes the origin of the host header to the target URL
+});
+
+
 // Create Server and run it.
-httpProxy.createServer(function (request, response,proxy) {
+http.createServer(function (request, response) {
     var decodedurl = decodeURI(request.url);
     if (decodedurl.indexOf('/api/') === 0)
     {
       // proxify url start with '/api/' to airvantage
-      proxy.proxyRequest(request, response, {
-      host: 'na.m2mop.net',
-      port: 80
-      });
+      proxy.proxyRequest(request, response);
     }
     else
     {
