6dfe0033d7430b5c000a3fe0bdca9a190ee0f3cc
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2013-03-01 12:09:05 +0100

    Better icons.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,15 +1,20 @@
 'use strict';
 
 /* Controllers */
-function GreenHouseCtrl($scope, $http, $timeout,$credentials) {
+function GreenHouseCtrl($scope, $http, $timeout, $credentials) {
+
+	function formatFirstValueFromTable( table ){
+		return parseInt(table[0].value).toFixed(2);
+	}
 
 	function tick() {
 		$http.get('/api/v1/systems/'+$credentials.system_id+'/data').success(function(data) {
 			$timeout(tick, 1000);
-			$scope.luminosity = data["modbus.data.luminosity"][0].value;
-			$scope.temperature = data["modbus.data.temperature"][0].value;
-			$scope.light = data["modbus.data.light"][0].value;
-			$scope.humidity = data["modbus.data.humidity"][0].value;			
+			$scope.light = data["modbus.data.light"][0].value === 'true';
+			$scope.lighticon = $scope.light ? 'icon-ok' : 'icon-remove';
+			$scope.luminosity  = formatFirstValueFromTable(data["modbus.data.luminosity"]);
+			$scope.temperature = formatFirstValueFromTable(data["modbus.data.temperature"]);
+			$scope.humidity    = formatFirstValueFromTable(data["modbus.data.humidity"]);			
 		});    
 	};
 	tick();
