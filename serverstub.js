4942c192d9a35c5fdae5ff752b3f2c702eecf5fc
Marc Aubry <maubry@sierrawireless.com>
2013-03-01 11:08:07 +0100

    Enhance serverstub and default credential behaviour

--- serverstub.js
+++ serverstub.js
@@ -40,15 +40,18 @@ httpProxy.createServer(function (request, response,proxy) {
       file.serve(request, response);
     }
   }
-).listen(80);
+).listen(9696);
 console.log ("Server Successfully Launched.".bold.blue)
 
 var responseCount = 0;
+var toogleBoolean = true;
 
 function getResponse(){
  responseCount = responseCount + 1;
- return {"modbus.data.luminosity":[{"value":(responseCount*4 % 200).toString(),"timestamp":1359715903713}],
-		"modbus.data.temperature":[{"value":(responseCount*3 % 45).toString(),"timestamp":1359715903713}],
-		"modbus.data.light":[{"value":(responseCount % 2).toString(),"timestamp":1359715903713}],
-		"modbus.data.humidity":[{"value":(responseCount*2 % 100).toString(),"timestamp":1359715903713}]}
+ toogleBoolean = !toogleBoolean;
+ return {"modbus.data.luminosity":[{"value":(responseCount % 75 * 2.66).toString(),"timestamp":1359715903713}],
+		"modbus.data.temperature":[{"value":(responseCount % 38 * 0.84).toString(),"timestamp":1359715903713}],
+		"modbus.data.light":[{"value":toogleBoolean.toString(),"timestamp":1359715903713}],
+		"modbus.data.open":[{"value":(!toogleBoolean).toString(),"timestamp":1359715903713}],
+		"modbus.data.humidity":[{"value":(responseCount % 24 * 4.16).toString(),"timestamp":1359715903713}]}
 }
