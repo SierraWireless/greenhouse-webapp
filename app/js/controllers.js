b558871745fbb05985cbb5ec6d4d8e23679d4368
Kevin KIN-FOO <kkinfoo@sierrawireless.com>
2013-03-08 18:04:02 +0100

    Sending commands to server.

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -3,13 +3,15 @@
 /* Controllers */
 function GreenHouseCtrl($scope, $http, $timeout, $credentials) {
 
-	function determineButtonStyle( table ){
-		if( table[0].value === 'true' )
+	function determineButtonStyle( status ){
+		if( status === 'true' )
 			return 'btn';
 		return 'btn active';
 	}
 
-	var assetName = "greenhouse"
+	var assetName = "greenhouse";
+	$scope.error   = '';
+	$scope.success = '';
 
 	function formatFirstValueFromTable( table ){
 		return parseFloat(table[0].value).toFixed(2);
@@ -21,17 +23,62 @@ function GreenHouseCtrl($scope, $http, $timeout, $credentials) {
 			$timeout(tick, 10000);
 
 			// Detemine button style from fresh values
-			$scope.lightbuttonclass = determineButtonStyle(data[assetName+".data.light"]);
-			$scope.shieldbuttonclass = determineButtonStyle(data[assetName+".data.open"]);
+			$scope.light = data[assetName+".data.light"][0].value === 'true';
+			$scope.lightbuttonclass = determineButtonStyle( $scope.light );
+			$scope.shield = data[assetName+".data.open"][0].value === 'true';
+			$scope.shieldbuttonclass = determineButtonStyle( $scope.shield ) ;
 
 			// Round given values
 			$scope.luminosity  = formatFirstValueFromTable(data[assetName+".data.luminosity"]);
 			$scope.temperature = formatFirstValueFromTable(data[assetName+".data.temperature"]);
-			$scope.humidity    = formatFirstValueFromTable(data[assetName+".data.humidity"]);			
+			$scope.humidity    = formatFirstValueFromTable(data[assetName+".data.humidity"]);
 
 		});    
 	};
+
 	tick();
+
+	$scope.toggleLight = function() {
+
+		// Fetch system info
+		$http.get('/api/v1/systems?uid=' + $credentials.system_id)
+			.success(function(data) {
+
+				// Extract application UID
+				var applicationUID = data.items[0].applications[0].uid;
+
+				/*
+				 * Send command
+				 */
+				var newLightStatus = ! $scope.light;
+				var postData = {
+					"application" : {
+						"uid" : applicationUID
+					},
+					"commandId"   : assetName+".data.switchLight",
+					"paramValues" : [
+						{
+							"key"   : "state",
+							"value" : newLightStatus
+						}
+					],
+					"systems" : {
+						"uids" : [$credentials.system_id]
+					},
+				};
+				$http.post('/api/v1/operations/systems/command', postData)
+				.success(function(data, status) {
+					$scope.success = "Switching light "+( newLightStatus ? 'on.' : 'off.' );
+				}).error(function(data, status, headers, config) {
+					$scope.error += 'Unable to switch light ' + ( newLightStatus ? 'on.' : 'off.');
+					console.log('Unable to change light status.');
+					console.log('Error ' + status + ', ' + data.error + '.');
+				});
+			}).error(function(data, staus){
+				console.log('Unable to fetch application UID.');
+				console.log('Error ' + status + ', ' + data.error + '.');
+			});
+	}
 }
 
 function DeviceStatusCtrl($scope, $http, $timeout, $credentials) {
